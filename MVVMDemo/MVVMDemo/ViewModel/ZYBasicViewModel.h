ca8c0c6a7eb1a2acee2bc2c847fda7bd5a9baa7e
lynulzy <lynulzy@163.com>
2015-08-22 16:21:07 +0800

    创建基础ViewModel类

---
+++ MVVMDemo/MVVMDemo/ViewModel/ZYBasicViewModel.h
@@ -0,0 +1,13 @@
+//
+//  ZYBasicViewModel.h
+//  MVVMDemo
+//
+//  Created by lynulzy on 8/22/15.
+//  Copyright (c) 2015 http://lynulzy.info. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZYBasicViewModel : NSObject
+
+@end
